e514805fc4fbb1c4675e2de6fa3fd94a5c3c4f4e
Siegfred Balona <siegblink@gmail.com>
2020-06-28 15:34:47 +0800

    Add a comment regarding unstable_createMuiStrictModeTheme.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import Main from './components/main/Main'
 // By using the unstable_createMuiStrictModeTheme,
 //   we are able to avoid the warning message
 //   in the console that mentions a deprecated findDomNode
-//   in React strict mode.
+//   in React strict mode. This is for development purpose only.
+// Once we move to production, we will replace
+//   unstable_createMuiStrictModeTheme with its stable version.
 const theme = createMuiTheme()
 
 function App() {
