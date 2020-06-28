fe67f93a8e36aa58f530482dc4f05df9de441152
Siegfred Balona <siegblink@gmail.com>
2020-06-28 15:08:26 +0800

    Add a comment regarding usage of unstable_createMuiStrictMode.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { unstable_createMuiStrictModeTheme as createMuiTheme } from '@material-u
 import { ThemeProvider } from '@material-ui/core/styles'
 import Main from './components/main/Main'
 
+// By using the unstable_createMuiStrictModeTheme,
+//   we are able to avoid the warning message
+//   in the console that mentions a deprecated findDomNode
+//   in React strict mode.
 const theme = createMuiTheme()
 
 function App() {
