debd3dc2eb31d64e709d6fc63dd52802237f1f00
Siegfred Balona <siegblink@gmail.com>
2020-07-05 23:27:06 +0800

    Add search text field in the header component.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import clsx from 'clsx'
-import { makeStyles } from '@material-ui/core/styles'
+import { makeStyles, fade } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import ToolBar from '@material-ui/core/ToolBar'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu'
 import AddIcon from '@material-ui/icons/Add'
+import SearchIcon from '@material-ui/icons/Search'
+import InputBase from '@material-ui/core/InputBase'
 import Typography from '@material-ui/core/Typography'
 
 const drawerWidth = 240
@@ -33,8 +35,46 @@ const useStyles = makeStyles(function (theme) {
       display: 'none',
     },
     headerTitle: {
-      flex: 1
-    }
+      flex: 1,
+    },
+    search: {
+      position: 'relative',
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: fade(theme.palette.common.white, 0.15),
+      '&:hover': {
+        backgroundColor: fade(theme.palette.common.white, 0.25),
+      },
+      marginLeft: 0,
+      [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto',
+      },
+    },
+    searchIcon: {
+      padding: theme.spacing(0, 2),
+      height: '100%',
+      position: 'absolute',
+      pointerEvents: 'none',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    inputRoot: {
+      color: 'inherit',
+    },
+    inputInput: {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        width: '12ch',
+        '&:focus': {
+          width: '20ch',
+        },
+      },
+    },
   }
 })
 
@@ -60,6 +100,7 @@ export default function Header(props) {
         <Typography variant='h6' noWrap className={classes.headerTitle}>
           Awesome Bookmark
         </Typography>
+        <SearchInputField />
         <IconButton
           aria-label='account of current user'
           aria-controls='menu-appbar'
@@ -70,5 +111,25 @@ export default function Header(props) {
         </IconButton>
       </ToolBar>
     </AppBar>
+  )
+}
+
+function SearchInputField() {
+  const classes = useStyles()
+
+  return (
+    <div className={classes.search}>
+      <div className={classes.searchIcon}>
+        <SearchIcon />
+      </div>
+      <InputBase
+        placeholder='Search…'
+        classes={{
+          root: classes.inputRoot,
+          input: classes.inputInput,
+        }}
+        inputProps={{ 'aria-label': 'search' }}
+      />
+    </div>
   )
 }
