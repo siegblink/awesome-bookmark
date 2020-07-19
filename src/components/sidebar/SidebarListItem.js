c6eaf38efd29a93087ced843ea3e8f982eb96a5a
Siegfred Balona <siegblink@gmail.com>
2020-07-19 20:04:56 +0800

    Refactor sidebar list item component.

--- src/components/sidebar/SidebarListItem.js
+++ src/components/sidebar/SidebarListItem.js
@@ -1,13 +1,23 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemIcon from '@material-ui/core/ListItemIcon'
 import ListItemText from '@material-ui/core/ListItemText'
 
+const useStyles = makeStyles(function () {
+  return {
+    listItem: {
+      paddingLeft: 24,
+    },
+  }
+})
+
 export default function SidebarListItem(props) {
+  const classes = useStyles()
   const { text, icon } = props.sidebarItem
 
   return (
-    <ListItem button>
+    <ListItem button className={classes.listItem}>
       <ListItemIcon>{icon}</ListItemIcon>
       <ListItemText primary={text} />
     </ListItem>
