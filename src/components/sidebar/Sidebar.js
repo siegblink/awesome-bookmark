4e5487e2933ddb2aa8dcccde2b6972e178091739
Siegfred Balona <siegblink@gmail.com>
2020-07-19 20:03:53 +0800

    Refactor sidebar component to implement a mini variant sidebar.

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import clsx from 'clsx'
 import { makeStyles, useTheme } from '@material-ui/core/styles'
 import Drawer from '@material-ui/core/Drawer'
+import Divider from '@material-ui/core/Divider'
 import IconButton from '@material-ui/core/IconButton'
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
 import ChevronRightIcon from '@material-ui/icons/ChevronRight'
@@ -12,44 +14,69 @@ const useStyles = makeStyles(function (theme) {
     drawer: {
       width: drawerWidth,
       flexShrink: 0,
+      whiteSpace: 'nowrap',
     },
-    drawPaper: {
+    drawerOpen: {
       width: drawerWidth,
+      transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
     },
-    drawerHeader: {
+    drawerClose: {
+      transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      overflowX: 'hidden',
+      width: theme.spacing(7) + 1,
+      [theme.breakpoints.up('sm')]: {
+        width: theme.spacing(9) + 1,
+      },
+    },
+    toolbar: {
       display: 'flex',
       alignItems: 'center',
-      padding: theme.spacing(0, 1),
-      // Necessary for content to be below AppBar
-      ...theme.mixins.toolbar,
       justifyContent: 'flex-end',
+      padding: theme.spacing(0, 1),
+      // necessary for content to be below app bar
+      ...theme.mixins.toolbar,
     },
   }
 })
 
 export default function Sidebar(props) {
-  const { open, handleDrawerClose } = props
   const classes = useStyles()
   const theme = useTheme()
+  const { open, handleDrawerClose } = props
 
   return (
-    <Drawer
-      variant='persistent'
-      anchor='left'
-      open={open}
-      classes={{ paper: classes.drawPaper }}
-      className={classes.drawer}
-    >
-      <div className={classes.drawerHeader}>
-        <IconButton onClick={handleDrawerClose}>
-          {theme.direction === 'ltr' ? (
-            <ChevronLeftIcon />
-          ) : (
-            <ChevronRightIcon />
-          )}
-        </IconButton>
-      </div>
-      {props.children}
-    </Drawer>
+    <Fragment>
+      <Drawer
+        variant='permanent'
+        className={clsx(classes.drawer, {
+          [classes.drawerOpen]: open,
+          [classes.drawerClose]: !open,
+        })}
+        classes={{
+          paper: clsx({
+            [classes.drawerOpen]: open,
+            [classes.drawerClose]: !open,
+          }),
+        }}
+      >
+        <div className={classes.toolbar}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? (
+              <ChevronRightIcon />
+            ) : (
+              <ChevronLeftIcon />
+            )}
+          </IconButton>
+        </div>
+        <Divider />
+        {props.children}
+      </Drawer>
+    </Fragment>
   )
 }
