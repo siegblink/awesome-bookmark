5b1a0264d114432ca15a2b56c81ab836dd9c6a8d
Siegfred Balona <siegblink@gmail.com>
2021-02-20 22:17:01 +0800

    Add 'OtherBookmarks' component.

---
+++ src/components/bookmarks/OtherBookmarks.js
@@ -0,0 +1,6 @@
+import EmptyBookmark from './EmptyBookmark'
+
+/** This is a component for displaying all 'Important' related bookmarks. */
+export default function OtherBookmarks(props) {
+  return <EmptyBookmark />
+}
