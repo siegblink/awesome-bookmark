cd20390e9603040d8ab2bee36f8daf23fc2785fe
Siegfred Balona <siegblink@gmail.com>
2021-02-20 22:14:27 +0800

    Add 'GithubBookmarks' component.

---
+++ src/components/bookmarks/GithubBookmarks.js
@@ -0,0 +1,6 @@
+import EmptyBookmark from './EmptyBookmark'
+
+/** This is a component for displaying all 'Github' related bookmarks. */
+export default function GithubBookmarks(props) {
+  return <EmptyBookmark />
+}
