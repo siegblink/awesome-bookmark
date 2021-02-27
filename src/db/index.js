955dd912acb8bbd5f2b7245fc81f49a8e07798c7
Siegfred Balona <siegblink@gmail.com>
2021-02-27 15:21:01 +0800

    Updated the 'shape' of dummy data.

--- src/db/index.js
+++ src/db/index.js
@@ -1,19 +1,26 @@
-const dummyData = [
-  {
-    name: 'github-octodex',
-    link: 'https://octodex.github.com',
-    category: 'Github',
-  },
-  {
-    name: 'mastering-markdown-github-guide',
-    link: 'https://guides.github.com/features/mastering-markdown/',
-    category: 'Github',
-  },
-  {
-    name: 'github-high-scores',
-    link: 'https://leereilly.net/github-high-scores/',
-    category: 'Github',
-  },
-]
+const dummyData = {
+  personal: [
+    {
+      name: 'github-octodex',
+      link: 'https://octodex.github.com',
+      category: 'Github',
+    },
+    {
+      name: 'mastering-markdown-github-guide',
+      link: 'https://guides.github.com/features/mastering-markdown/',
+      category: 'Github',
+    },
+    {
+      name: 'github-high-scores',
+      link: 'https://leereilly.net/github-high-scores/',
+      category: 'Github',
+    },
+  ],
+  github: [],
+  important: [],
+  libraries: [],
+  tools: [],
+  others: []
+}
 
 export default dummyData
