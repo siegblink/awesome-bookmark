771a64a35700d9d0e5e0f7b68b674e14d9abf172
Siegfred Balona <siegblink@gmail.com>
2021-03-05 09:09:01 +0800

    Integrate 'SearchDialog' in 'SearchButton' component.

--- src/components/header/SearchButton.js
+++ src/components/header/SearchButton.js
@@ -1,7 +1,9 @@
+import { useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import IconButton from '@material-ui/core/IconButton'
 import Tooltip from '@material-ui/core/Tooltip'
 import SearchIcon from '@material-ui/icons/Search'
+import SearchDialog from '../dialog/SearchDialog'
 
 const useStyles = makeStyles(function (theme) {
   return {
@@ -21,20 +23,31 @@ const useStyles = makeStyles(function (theme) {
 
 export default function SearchButton() {
   const classes = useStyles()
+  const [isDialogOpen, setIsDialogOpen] = useState(false)
+
+  function openDialog() {
+    setIsDialogOpen(true)
+  }
+
+  function closeDialog() {
+    setIsDialogOpen(false)
+  }
 
   return (
     <div>
       <Tooltip arrow title='Add new bookmark'>
         <IconButton
-          aria-label='add a new bookmark'
+          aria-label='search-button'
           aria-controls='menu-appbar'
           aria-haspopup='true'
           color='inherit'
+          onClick={openDialog}
           className={classes.iconButton}
         >
           <SearchIcon />
         </IconButton>
       </Tooltip>
+      <SearchDialog isDialogOpen={isDialogOpen} closeDialog={closeDialog} />
     </div>
   )
 }
