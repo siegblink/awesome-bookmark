4c0b7f36fc4db8015c1fab5c1415300330d906d1
Siegfred Balona <siegblink@gmail.com>
2021-03-06 11:53:36 +0800

    Add 'Back button' in 'LeftSideDrawer'.

--- src/components/sidebar/LeftSideDrawer.js
+++ src/components/sidebar/LeftSideDrawer.js
@@ -1,4 +1,8 @@
 import { makeStyles } from '@material-ui/core/styles'
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
+import IconButton from '@material-ui/core/IconButton'
+import Tooltip from '@material-ui/core/Tooltip'
+import Divider from '@material-ui/core/Divider'
 import Drawer from '@material-ui/core/Drawer'
 
 const drawerWidth = 240
@@ -12,8 +16,14 @@ const useStyles = makeStyles((theme) => {
     },
     listContainer: {
       display: 'flex',
-      flexDirection: 'column',
       flex: 1,
+      flexDirection: 'column',
+      padding: theme.spacing(0, 1),
+    },
+    backButton: {
+      margin: theme.spacing(1, 0),
+      width: 'fit-content',
+      alignSelf: 'flex-end',
     },
   }
 })
@@ -30,6 +40,17 @@ export default function RightSideDrawer(props) {
       className={classes.drawer}
     >
       <div className={classes.listContainer} role='presentation'>
+        {/* Back button */}
+        <Tooltip arrow title='Close menu' placement='left'>
+          <IconButton
+            onClick={closeDrawer}
+            classes={{ root: classes.backButton }}
+          >
+            <ChevronLeftIcon />
+          </IconButton>
+        </Tooltip>
+
+        <Divider />
         {children}
       </div>
     </Drawer>
