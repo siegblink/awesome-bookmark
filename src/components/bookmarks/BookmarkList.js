0b664fe13f6f9c706a84bd73af9a7e211d8e2995
Siegfred Balona <siegblink@gmail.com>
2021-03-09 23:36:41 +0800

    Create generic 'BookmarkList' component to display a list of bookmarks.

---
+++ src/components/bookmarks/BookmarkList.js
@@ -0,0 +1,33 @@
+import { useEffect, useContext } from 'react'
+import { BookmarkContext } from '../../context/'
+import EmptyBookmark from './EmptyBookmark'
+import Bookmark from '../content/Bookmark'
+
+/** This is a component for displaying all 'Important' related bookmarks. */
+export default function BookmarkList(props) {
+  const { bookmarkGroup } = props
+
+  // Get the 'Bookmark' context data.
+  const context = useContext(BookmarkContext)
+  const { bookmarks, dispatch, openRightSideDrawer, updatePathname } = context
+
+  // Execute side effect when browser mounts this component.
+  useEffect(() => {
+    updatePathname(bookmarkGroup)
+  }, [updatePathname, bookmarkGroup])
+
+  return !bookmarks[bookmarkGroup].length ? (
+    <EmptyBookmark />
+  ) : (
+    bookmarks[bookmarkGroup].map((bookmark) => {
+      return (
+        <Bookmark
+          key={bookmark.name}
+          bookmark={bookmark}
+          openEditDrawer={openRightSideDrawer}
+          dispatch={dispatch}
+        />
+      )
+    })
+  )
+}
